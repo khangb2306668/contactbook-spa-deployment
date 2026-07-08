a10dedd1891d7212e3c348b4ba5d40e19befff8c
Ho Vi Khang <khangb2306668@student.ctu.edu.vn>
2026-07-08 17:09:21 +0700

    sửa lỗi avatar url và deploy

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/contactbook-spa-deployment/sw.js', { scope: '/contactbook-spa-deployment/' })})}
